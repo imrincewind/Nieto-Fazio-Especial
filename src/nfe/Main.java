b4157c7af2d91fba1617707fbe25b49aa61193d3
Ezequiel Fazio <ezfazio@gmail.com>
2018-05-03 17:49:48 -0300

    agregado el timer

--- src/nfe/Main.java
+++ src/nfe/Main.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		String pathEntrada = "C:/TUDAI-ws/Nieto-Fazio-Especial/datasets/dataset2.csv";
+		String pathEntrada = "C:/TUDAI-ws/Nieto-Fazio-Especial/datasets/dataset4.csv";
 		String pathSalida = "C:/TUDAI-ws/Nieto-Fazio-Especial/datasets/salida.csv";
-		
+		Timer tiempo = new Timer();
 		BUSCADOR b = new BUSCADOR();
 		
 		CSVReader.main(args, b, pathEntrada);
-		ArrayList<Libro> librosFiltrados = b.porGenero("juegfgjud");
+		tiempo.start();
+		ArrayList<Libro> librosFiltrados = b.porGenero("juegos");
+		System.out.println(tiempo.stop());
 		CSVWritter.main(args, librosFiltrados, pathSalida);
 	}
 
